0eb8459d36c58b5ef110e10c066d2a7196e7f891
henry199101 <331243543@qq.com>
2018-06-15 16:11:31 +0800

    19.3.4 保护用户的主题 .

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -22,6 +22,9 @@ def topics(request):
 def topic(request, topic_id):
 	"""显示单个主题及其所有的条目"""
 	topic = Topic.objects.get(id=topic_id)
+	# 确认请求的主题术语当前用户
+	if topic.owner != request.user:
+		raise Http404
 	entries = topic.entry_set.order_by('-date_added')
 	context = {'topic': topic, 'entries': entries}
 	return render(request, 'learning_logs/topic.html', context)
