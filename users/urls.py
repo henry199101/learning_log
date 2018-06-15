a6d7aa5095cb5fd7deea22cdc2c3ae637044a18e
henry199101 <331243543@qq.com>
2018-06-15 14:30:33 +0800

    19.2.2 登录页面: 在 learning_log/users/ 中,新建 urls.py 文件.

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.contrib.auth.views import login
+
+from . import views
+
+urlpatterns = [
+	# 登录页面
+	url(r'^login/$', login, {'template_name': 'users/login.html'},
+		name='login'),
+]
+app_name = "users"
