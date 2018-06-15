cd630f01ec80ba7e5afc622444066cc146cb1926
henry199101 <331243543@qq.com>
2018-06-15 10:19:53 +0800

    19.1.1 添加新主题 - 2. URL 模式 new_topic .

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
 	# 显示所有的主题
 	url(r'^topics/$', views.topics, name='topics'),
 	url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+	# 用于添加新主题的网页
+	url(r'^new_topic/$', views.new_topic, name='new_topic'),
 ]
 app_name = "learning_logs"
