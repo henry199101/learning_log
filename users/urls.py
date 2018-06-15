b167ac1c8107f05f36fa9512d547a05bb0a239c0
henry199101 <331243543@qq.com>
2018-06-15 14:52:17 +0800

    19.2.3 注销 - 1.注销URL: 为注销定义 URL 模式 .

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
 	# 登录页面
 	url(r'^login/$', login, {'template_name': 'users/login.html'},
 		name='login'),
+	# 注销
+	url(r'^logout/$', views.logout_view, name='logout'),
 ]
 app_name = "users"
